cbd3a94cb41ed48bd630673ea8432da2c6d2848a
SOMYA6269 <modisomy62@gmail.com>
2025-11-19 23:22:47 +0530

    Update email.js

--- routes/email.js
+++ routes/email.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import SibApiV3Sdk from 'sib-api-v3-sdk'
+import { createRequire } from 'module'
 import dotenv from 'dotenv'
 import { sendContactEmail } from '../services/email.js'
 
 dotenv.config()
+
+// Use createRequire to import CommonJS module in ES modules
+const require = createRequire(import.meta.url)
+const SibApiV3Sdk = require('sib-api-v3-sdk')
 
 export const emailRouter = express.Router()
 
